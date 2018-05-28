9e82ee7603df96e1e6f2d3693891a9acbc0ab50c
Sergio Sanz Llamas <sanzsergio4ab@gmail.com>
2018-05-28 14:09:17 +0200

    insertar

--- lenguajemarcas/tema5javascript/script/agenda.js
+++ lenguajemarcas/tema5javascript/script/agenda.js
@@ -1,16 +1,24 @@
 class Contacto{
-	constructor(n, t){
+	constructor(n, t,d){
 		this.nombre = n;
 		this.telefono = t;
+		this.direccion = d;
+
 	}
 
 
-Escribir(){
+Escribir(idhtml){
 var resultado;
-resultado = document.getElementById('Resultado');
-resultado.innerHTML = 
+resultado = document.getElementById(idhtml);
+resultado.innerHTML += 
 "Nombre:" + this.nombre + "<br>" +
-"Telefono:" + this.telefono + "<br";
+"Telefono:" + this.telefono + "<br>" + 
+"direccion:" + this.direccion + "<br>";
+}
+EscribirNombre(idhtml){
+	var resultado;
+	resultado = document.getElementById(idhtml);
+	resultado.innerHTML += "Nombre: " + this.nombre +"<br>"; 
 }
 CambiarTelefono(nuevoT){
 	this.telefono = nuevoT;
@@ -18,9 +26,42 @@ CambiarTelefono(nuevoT){
 CambiarNombre(nuevoN){
 	this.nombre = nuevoN;
  }
+ CambiarNombre(nuevaD){
+	this.direccion = nuevaD;
+ }
+}
+class ListaContactos{
+		constructor(){
+		this.nContactos = 0;
+		this.lista = new Array(5);
+		
+
+	}
+	InsertarContacto(n,t,d){
+		var c1 = new Contacto(n,t,d);
+		this.lista[this.nContactos] = c1;
+		this.nContactos++;
+
+	}
+	EscribirNombres(idhtml){
+			var resultado;
+	resultado = document.getElementById(idhtml);
+		resultado.innerHTML = " ";
+		for (var i = 0; i < this.nContactos; i++) {
+			this.lista[i].EscribirNombre(idhtml);
+		}
+	}
 }
 
-var c1;
-var c1;
-var c1;
-var c1;
+var miLista = new ListaContactos();
+
+function InsertarContactoLista() {
+	var miNombre, miTelefono,miDireccion;
+
+	miNombre = document.getElementById('nombre').value;
+	miTelefono = document.getElementById('telefono').value;
+	miDireccion = document.getElementById('direccion').value;
+	miLista.InsertarContacto(miNombre,miDireccion,miTelefono);
+	miLista.EscribirNombres('ListaContactoNombre');
+
+	}
